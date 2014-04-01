ee0273b4d7fbcb0ecaf5aa9da6a9cac9db3c2781
huchao <295971796@qq.com>
2014-04-01 15:54:06 +0800

    、。。。。

--- src/com/oj/jxc/commons/ContextLoaderListenerOverWrite.java
+++ src/com/oj/jxc/commons/ContextLoaderListenerOverWrite.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.oj.jxc.commons;
 
 import javax.servlet.ServletContextEvent;
@@ -9,30 +6,28 @@ import org.springframework.web.context.ContextLoaderListener;
 
 /**
  * @author zxd
- *
+ * 
  */
 public class ContextLoaderListenerOverWrite extends ContextLoaderListener {
-	
-	
-	public void contextInitialized(ServletContextEvent event) {  
-		 super.contextInitialized(event);  
-		 
-		 /*
-		 ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
-		 dicItemService=(DicItemService) applicationContext.getBean("dicItemService");
-		 orgService=(OrgService) applicationContext.getBean("orgService");
-		 
-		 GlobalDefine.CacheBasisData.orgForMap=orgService.getOrgIdForLevel(2);
- 
-		 //字典表
-		 Map<String,Map<String,String>> dicItem=new HashMap<String,Map<String,String>>();
-		 List<String> dicTypeList=dicItemService.getDicTypeList();
-		 for(String typeId:dicTypeList){
-			 Map<String,String> map=dicItemService.getDicItemByDicType(typeId);
-			 dicItem.put(typeId, map);
-		 }
-		 GlobalDefine.CacheBasisData.dicItem=dicItem;
-		 
+
+	public void contextInitialized(ServletContextEvent event) {
+		super.contextInitialized(event);
+
+		/*
+		 * ApplicationContext applicationContext =
+		 * WebApplicationContextUtils.getWebApplicationContext
+		 * (event.getServletContext()); dicItemService=(DicItemService)
+		 * applicationContext.getBean("dicItemService"); orgService=(OrgService)
+		 * applicationContext.getBean("orgService");
+		 * 
+		 * GlobalDefine.CacheBasisData.orgForMap=orgService.getOrgIdForLevel(2);
+		 * 
+		 * //字典表 Map<String,Map<String,String>> dicItem=new
+		 * HashMap<String,Map<String,String>>(); List<String>
+		 * dicTypeList=dicItemService.getDicTypeList(); for(String
+		 * typeId:dicTypeList){ Map<String,String>
+		 * map=dicItemService.getDicItemByDicType(typeId); dicItem.put(typeId,
+		 * map); } GlobalDefine.CacheBasisData.dicItem=dicItem;
 		 */
 	}
 }
